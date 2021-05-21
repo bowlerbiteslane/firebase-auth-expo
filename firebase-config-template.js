c42fe687615539d66957107063cbb3d546f824f0
Ethan Currah <ecurrah90@gmail.com>
2021-05-20 20:07:03 -0400

    Added firebase config template for future reference. Will be adding actual config to gitignore

--- firebase-config-template.js
+++ firebase-config-template.js
@@ -6,5 +6,5 @@ export default firebaseConfig = {
     storageBucket: "YOUR_FIREBASE_STORAGE_BUCKET",
     messagingSenderId: "YOUR_FIREBASE_MESSAGING_SENDER_ID",
     appId: "YOUR_FIREBASE_APP_ID",
-    measurementId: "YOUR_FIREBASE_MEASUREMENT_ID"
+    measurementId: "YOUR_FIREBASE_MEASUREMENT_ID_ID"
 }
